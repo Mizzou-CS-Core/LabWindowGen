eb11c3c1d51a3b07a49ac0541ed9e24cb5fb9566
Matt Marlow <thequantumblink@gmail.com>
2025-04-30 17:28:39 -0500

    fix: setup paths and updated docs

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name="gen_assignment_window",
-  version="0.3.8",
+  version="0.4.0",
   packages=find_packages(include=["gen_assignment_window", "gen_assignment_window.*"]),
   install_requires=[
   "canvas_lms_api @ git+https://github.com/Mizzou-CS-Core/CanvasRequestLibrary.git#egg=canvas_lms_api",
